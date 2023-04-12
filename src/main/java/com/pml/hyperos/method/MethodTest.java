f5d7c747b1f56b5a3886a7c4dbad6bac7c1fe3f7
william-lbn <909445583.com>
2023-04-12 15:06:04 +0800

    demo init

---
+++ src/main/java/com/pml/hyperos/method/MethodTest.java
@@ -0,0 +1,31 @@
+package com.pml.hyperos.method;
+
+/**
+ * @author： libin
+ * @email： 909445583@qq.com
+ * @date： 2023/4/6
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class MethodTest {
+
+    private String name = "hello private name=123";
+
+
+
+
+    public void printName(){
+
+        System.out.print(name);
+
+    }
+
+    public void printName(String str){
+
+        System.out.print(str);
+
+    }
+
+
+}
---
+++ src/main/java/com/pml/hyperos/test/Test.java
@@ -0,0 +1,26 @@
+package com.pml.hyperos.test;
+
+import com.pml.hyperos.method.MethodTest;
+
+/**
+ * @author： libin
+ * @email： 909445583@qq.com
+ * @date： 2023/4/6
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        MethodTest methodTest = new MethodTest();
+
+
+        methodTest.printName();
+
+
+    }
+
+}
